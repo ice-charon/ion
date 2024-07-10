36d93375b6c33bcc934fde191907a591845919c6
ice-charon <antoniy.shumanov@leftclick.io>
2024-07-10 17:05:04 +0300

    Add create-hardfork to ubuntu builds
    Signed-off-by: ice-charon <antoniy.shumanov@leftclick.io>

--- assembly/native/build-ubuntu-shared.sh
+++ assembly/native/build-ubuntu-shared.sh
@@ -55,7 +55,7 @@ if [ "$with_tests" = true ]; then
 ninja storage-daemon storage-daemon-cli fift func tonlib tonlibjson tonlib-cli \
       validator-engine lite-client pow-miner validator-engine-console blockchain-explorer \
       generate-random-id json2tlo dht-server http-proxy rldp-http-proxy \
-      adnl-proxy create-state emulator test-ed25519 test-ed25519-crypto test-bigint \
+      adnl-proxy create-state create-hardfork emulator test-ed25519 test-ed25519-crypto test-bigint \
       test-vm test-fift test-cells test-smartcont test-net test-tdactor test-tdutils \
       test-tonlib-offline test-adnl test-dht test-rldp test-rldp2 test-catchain \
       test-fec test-tddb test-db test-validator-session-state
@@ -64,7 +64,7 @@ else
 ninja storage-daemon storage-daemon-cli fift func tonlib tonlibjson tonlib-cli \
       validator-engine lite-client pow-miner validator-engine-console blockchain-explorer \
       generate-random-id json2tlo dht-server http-proxy rldp-http-proxy \
-      adnl-proxy create-state emulator
+      adnl-proxy create-state create-hardfork emulator
       test $? -eq 0 || { echo "Can't compile ton"; exit 1; }
 fi
 
